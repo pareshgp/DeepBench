bbcf4091073b4bba78ccfe3614f16eed90edfd16
shomy sanyal <shomy.sanyal@amd.com>
2019-06-19 11:31:35 +0000

    max tensor core perf: half precision, 8K matrices

--- code/kernels/gemm_problems.h
+++ code/kernels/gemm_problems.h
@@ -1,6 +1,9 @@
 // Vector saves m, n, k, a_t, b_t
 std::vector<std::tuple<int, int, int, bool, bool>> training_set = {
+    std::make_tuple(8192, 8192, 8192, false, false),
+    std::make_tuple(8192, 8192, 8192, false, true),
     std::make_tuple(4096, 4096, 4096, false, false),
+    std::make_tuple(4096, 4096, 4096, false, true),
     std::make_tuple(1024, 1024, 1024, false, false),
     std::make_tuple(512, 512, 512, false, false),
 /*
