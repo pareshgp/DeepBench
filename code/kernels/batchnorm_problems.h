1f24e4dd164577765c1b8ce4752c491d9b1980ca
aayushmaanjain <aayushmaan.jain42@gmail.com>
2019-10-01 12:18:46 +0000

    Batchnorm problem sizes for resnet101, resnet152

--- code/kernels/batchnorm_problems.h
+++ code/kernels/batchnorm_problems.h
@@ -2,13 +2,67 @@
 std::vector<std::tuple<unsigned int, unsigned int, unsigned int, unsigned int,
                        unsigned int, double, double>> training_set = {
 
-// ResNet
-std::make_tuple(112, 112, 64, 112, 64, 0.1, 0.00001)
+// ResNet101
+std::make_tuple(112,112,64,112,64,0.1,1e-05),
+std::make_tuple(56,56,64,112,64,0.1,1e-05),
+std::make_tuple(56,56,256,112,256,0.1,1e-05),
+std::make_tuple(56,56,128,112,128,0.1,1e-05),
+std::make_tuple(28,28,128,112,128,0.1,1e-05),
+std::make_tuple(28,28,512,112,512,0.1,1e-05),
+std::make_tuple(28,28,256,112,256,0.1,1e-05),
+std::make_tuple(14,14,256,112,256,0.1,1e-05),
+std::make_tuple(14,14,1024,112,1024,0.1,1e-05),
+std::make_tuple(14,14,512,112,512,0.1,1e-05),
+std::make_tuple(7,7,512,112,512,0.1,1e-05),
+std::make_tuple(7,7,2048,112,2048,0.1,1e-05)
+
+// Resnet152
+std::make_tuple(112,112,64,80,64,0.1,1e-05),
+std::make_tuple(56,56,64,80,64,0.1,1e-05),
+std::make_tuple(56,56,256,80,256,0.1,1e-05),
+std::make_tuple(56,56,128,80,128,0.1,1e-05),
+std::make_tuple(28,28,128,80,128,0.1,1e-05),
+std::make_tuple(28,28,512,80,512,0.1,1e-05),
+std::make_tuple(28,28,256,80,256,0.1,1e-05),
+std::make_tuple(14,14,256,80,256,0.1,1e-05),
+std::make_tuple(14,14,1024,80,1024,0.1,1e-05),
+std::make_tuple(14,14,512,80,512,0.1,1e-05),
+std::make_tuple(7,7,512,80,512,0.1,1e-05),
+std::make_tuple(7,7,2048,80,2048,0.1,1e-05)
 
 };
 
+
 std::vector<std::tuple<unsigned int, unsigned int, unsigned int, unsigned int,
                        unsigned int, double, double>> inference_server_set = {
+
+// ResNet101
+std::make_tuple(112,112,64,112,64,0.1,1e-05),
+std::make_tuple(56,56,64,112,64,0.1,1e-05),
+std::make_tuple(56,56,256,112,256,0.1,1e-05),
+std::make_tuple(56,56,128,112,128,0.1,1e-05),
+std::make_tuple(28,28,128,112,128,0.1,1e-05),
+std::make_tuple(28,28,512,112,512,0.1,1e-05),
+std::make_tuple(28,28,256,112,256,0.1,1e-05),
+std::make_tuple(14,14,256,112,256,0.1,1e-05),
+std::make_tuple(14,14,1024,112,1024,0.1,1e-05),
+std::make_tuple(14,14,512,112,512,0.1,1e-05),
+std::make_tuple(7,7,512,112,512,0.1,1e-05),
+
+// Resnet152
+std::make_tuple(112,112,64,80,64,0.1,1e-05),
+std::make_tuple(56,56,64,80,64,0.1,1e-05),
+std::make_tuple(56,56,256,80,256,0.1,1e-05),
+std::make_tuple(56,56,128,80,128,0.1,1e-05),
+std::make_tuple(28,28,128,80,128,0.1,1e-05),
+std::make_tuple(28,28,512,80,512,0.1,1e-05),
+std::make_tuple(28,28,256,80,256,0.1,1e-05),
+std::make_tuple(14,14,256,80,256,0.1,1e-05),
+std::make_tuple(14,14,1024,80,1024,0.1,1e-05),
+std::make_tuple(14,14,512,80,512,0.1,1e-05),
+std::make_tuple(7,7,512,80,512,0.1,1e-05),
+std::make_tuple(7,7,2048,80,2048,0.1,1e-05)
+std::make_tuple(7,7,2048,112,2048,0.1,1e-05)
 
 };
 
